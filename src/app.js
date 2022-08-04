251417173dd283d3655fbd0dfcfe5c481c5ee620
NicolasCoelho <nicolas_mansan@hotmail.com>
2022-08-04 00:29:13 -0300

    Fix on session on production

--- src/app.js
+++ src/app.js
@@ -4,14 +4,20 @@ const views = require('./views')
 const routes = require('./routes')
 const middlewares = require('./middleware')
 
+const session = require('express-session')
+const sessionConfig = { 
+  secret: process.env.SESSION_SECRET, 
+  cookie: { maxAge: 60000 }
+}
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1)
+  sessionConfig.cookie.secure = true
+}
+app.use(session(sessionConfig))
+
 app.use(middlewares)
 app.use(views)
 app.use(routes)
-
-if (app.get('env') === 'production') {
-  app.set('trust proxy', 1)
-  session.cookie.secure = true
-}
 
 const port = process.env.PORT
 app.listen(port, () => console.log(`Server listening on port ${port}`))
--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const helmet = require('helmet')
-const session = require('express-session')
 
 router.use(express.urlencoded())
 router.use(express.json())
 router.use(helmet())
-router.use(session({ secret: process.env.SESSION_SECRET, cookie: { maxAge: 60000 }}))
 
 module.exports = router
